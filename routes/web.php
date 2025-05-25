9b55cb1adf857b7c0941e6ba1b32e5d450874276
Maulanaputra07 <maulanaaputraaa077@gmail.com>
2025-05-25 20:58:50 +0700

    1

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ use App\Http\Controllers\BlogController;
 use App\Http\Controllers\AdminController;
 
 Route::get('/', [ShowDataController::class, 'index']);
-Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
-Route::post('/admin', [AdminController::class, 'store'])->name('admin.store');
-Route::put('/admin/{id}', [AdminController::class, 'update'])->name('admin.update');
-Route::delete('/admin/{id}', [AdminController::class, 'destroy'])->name('admin.destroy');
+// Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+// Route::post('/admin', [AdminController::class, 'store'])->name('admin.store');
+// Route::put('/admin/{id}', [AdminController::class, 'update'])->name('admin.update');
+// Route::delete('/admin/{id}', [AdminController::class, 'destroy'])->name('admin.destroy');
 Route::get('/blog/{id}', [BlogController::class, 'show'])->name('blog.show');
